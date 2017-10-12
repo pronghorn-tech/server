89225ced8a9e8ee8bb8fda1ca68002f8e58e5619
Chris Turner <chris@pronghorn.tech>
2017-10-12 14:13:55 -0600

    Moved HttpResponseHeaderValuePair class to its own file.

--- src/main/kotlin/tech/pronghorn/http/HttpResponseHeaderValue.kt
+++ src/main/kotlin/tech/pronghorn/http/HttpResponseHeaderValue.kt
@@ -80,18 +80,3 @@ class ByteArrayResponseHeaderValue(override val value: ByteArray) : HttpResponse
     }
 }
 
-data class HttpResponseHeaderValuePair(val type: HttpResponseHeader,
-                                       val value: HttpResponseHeaderValue<*>) {
-    val length = type.displayBytes.size + value.valueLength + 4 // "header: value\r\n"
-
-    constructor(headerType: HttpResponseHeader,
-                value: ByteArray) : this(headerType, HttpResponseHeaderValue.valueOf(value))
-
-    constructor(headerType: HttpResponseHeader,
-                value: Int) : this(headerType, HttpResponseHeaderValue.valueOf(value))
-
-    constructor(headerType: HttpResponseHeader,
-                value: String) : this(headerType, HttpResponseHeaderValue.valueOf(value))
-
-    fun writeHeader(buffer: ByteBuffer) = value.writeHeader(type, buffer)
-}
